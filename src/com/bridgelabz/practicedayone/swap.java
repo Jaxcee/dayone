38be5f55f562fcde993d9aa0439563718e7f08f5
JAGADESH J <you@example.com>
2024-02-03 14:48:46 +0530

    swap

---
+++ src/com/bridgelabz/practicedayone/swap.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.practicedayone;
+
+public class swap {
+    public static void main(String[] args) {
+        int fn=5;
+        int sn=6;
+
+        System.out.println(fn);
+        System.out.println(sn);
+        int temp=fn;
+        fn=sn;
+        sn=temp;
+        System.out.println(fn);
+        System.out.println(sn);
+
+
+
+    }
+}
