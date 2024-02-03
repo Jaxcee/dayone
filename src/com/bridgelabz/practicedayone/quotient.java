08bf869b3341897c597d133cd9d1a5f8f4b85f09
JAGADESH J <you@example.com>
2024-02-03 14:30:02 +0530

    quotient

---
+++ src/com/bridgelabz/practicedayone/quotient.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.practicedayone;
+
+public class quotient {
+    public static void main(String[] args) {
+        int n=5;
+        int a=25;
+        int quo=a/n;
+        int rem = a%n;
+        System.out.println(quo);
+        System.out.println(rem);
+    }
+}
