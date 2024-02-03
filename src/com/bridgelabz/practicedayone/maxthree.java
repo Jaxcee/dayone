fd560ddb23eecb48e62181bb81f3aee0d4fd1f23
JAGADESH J <you@example.com>
2024-02-03 15:06:10 +0530

    maxthree

---
+++ src/com/bridgelabz/practicedayone/maxthree.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.practicedayone;
+
+public class maxthree {
+    public static void main(String[] args) {
+        int n=10;
+        int m=12;
+        int o=5;
+        if(n>m && n>o){
+            System.out.println("n is max");
+        } else if (m>n && m>o) {
+            System.out.println("m is max");
+
+        }else{
+            System.out.println("o is max");
+        }
+    }
+}
