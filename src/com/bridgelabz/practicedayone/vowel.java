f3bbd7978c3988149737b103621b6f8af12671ec
JAGADESH J <you@example.com>
2024-02-03 15:01:05 +0530

    vowel

---
+++ src/com/bridgelabz/practicedayone/vowel.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.practicedayone;
+
+public class vowel {
+    public static void main(String[] args) {
+        char ch = 'i';
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+            System.out.println("vowel");
+        } else{
+            System.out.println("not vowel");
+
+            }
+        }
+
+    }
+
